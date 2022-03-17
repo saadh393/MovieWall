5ce7408cb122a33f99375494525e7ee831bd400b
Saad Hasan <saadh393@gmail.com>
2022-03-17 23:24:49 +0600

    Updated SDK

--- app/src/main/java/com/wwinc/moviewall/SplashScreen/Splash_Screen.kt
+++ app/src/main/java/com/wwinc/moviewall/SplashScreen/Splash_Screen.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.wwinc.moviewall.MainActivity
 import com.wwinc.moviewall.Model.EntityWallpaper
@@ -40,7 +43,7 @@ class Splash_Screen : AppCompatActivity() {
 
 
 //        // Internet Connection
-        if (!isNetworkConnected()) {
+        if (!isNetworkConnected(applicationContext)) {
             showDialog()
         }else{
 
@@ -61,10 +64,35 @@ class Splash_Screen : AppCompatActivity() {
 
     }
 
-    private fun isNetworkConnected(): Boolean {
-        val cm =
-            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        return cm.activeNetworkInfo != null && cm.activeNetworkInfo.isConnected
+    private fun isNetworkConnected(context: Context): Boolean {
+        var result = false
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectivityManager.activeNetwork ?: return false
+            val actNw =
+                connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+            result = when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.run {
+                connectivityManager.activeNetworkInfo?.run {
+                    result = when (type) {
+                        ConnectivityManager.TYPE_WIFI -> true
+                        ConnectivityManager.TYPE_MOBILE -> true
+                        ConnectivityManager.TYPE_ETHERNET -> true
+                        else -> false
+                    }
+
+                }
+            }
+        }
+
+        return result
     }
 
     fun showDialog(){
